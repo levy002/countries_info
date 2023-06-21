c0ae50ea26083337774d431e2e136ea469c5718b
“levy-DELL” <levy.ukwi002@gmail.com>
2023-06-22 01:05:29 +0200

    Render all countries list on the page

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Countries } from './features/countries/Countries';
+
+function App() {
+  return (
+    <div className="App">
+      <Countries />
+    </div>
+  );
+}
+
+export default App;
